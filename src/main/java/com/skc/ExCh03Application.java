70da694fa46918bb912fc09745f3068598d7026e
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-10-29 11:40:47 +0900

    edit 주석

--- src/main/java/com/skc/ExCh03Application.java
+++ src/main/java/com/skc/ExCh03Application.java
@@ -1,9 +1,7 @@
 package com.skc;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 //@ComponentScan(basePackages = { "com.skc.demo", "com.skc.mybatis", "com.skc.ex_thymleaf" })
--- src/main/java/com/skc/mybatis/controller/StudentController.java
+++ src/main/java/com/skc/mybatis/controller/StudentController.java
@@ -5,15 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skc.mybatis.domain.Student;
 import com.skc.mybatis.service.StudentService;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/students")
