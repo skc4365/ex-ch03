b68964eebd20d2b459e41a9b109f9a65c57337db
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-10-29 11:41:16 +0900

    add TDD테스트코드

--- src/test/java/com/skc/ExCh03ApplicationTests.java
+++ src/test/java/com/skc/ExCh03ApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.skc;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExCh03ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
